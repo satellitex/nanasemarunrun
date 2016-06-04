c7e96662bd3fe19aa585047725a6c9af8a1489dc
satellitex <s1220245@u-aizu.ac.jp>
2016-06-04 08:35:07 +0000

    no change

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
-/*Rails.application.routes.draw do
-  root 'application#hello'
-end
-*/
-
 Rails.application.routes.draw do
   namespace :api, default: {format: :json} do
     resources :chats, only: :create
