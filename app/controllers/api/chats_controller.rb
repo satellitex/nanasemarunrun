b16f127c36dc448ee8801fc82bee61742e670136
satellitex <s1220245@u-aizu.ac.jp>
2016-06-04 10:24:47 +0000

    shine

--- app/controllers/api/chats_controller.rb
+++ app/controllers/api/chats_controller.rb
@@ -1,7 +1,3 @@
-/*class Api::ChatsController < ApplicationController
-end
-*/
-
 class Api::ChatsController < ApplicationController
   skip_before_filter :verify_authenticity_token #外部からのリクエストを許可する
   def create
